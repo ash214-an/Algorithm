8371d9a28c5bca1dbc79ad42795d7431551c0937
Sungho an <intimacy222@naver.com>
2025-07-11 09:47:21 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int sum = 0;
+    for(int i = 0; i < absolutes.size(); i++)
+    {
+        sum += absolutes[i] * (signs[i]*1 == 1 ? 1 : -1);
+    }
+    return sum;
+}
